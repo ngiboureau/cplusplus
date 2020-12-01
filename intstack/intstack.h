54e794030e2000ca6fdc087155371b3a37f02231
ngiboureau <nils.giboureau@mines-paristech.fr>
2020-12-01 15:55:33 +0100

    modifications et tests

--- intstack/intstack.h
+++ intstack/intstack.h
@@ -11,7 +11,7 @@ class IntStack
   
         IntStack (int a, ...) //a est le nombre de valeurs initiales
         {
-            std::vector<int>* init = new std::vector<int>;
+            std::vector<int> init;
             int i=0,j=a;
             va_list ap;
             va_start(ap,a);
@@ -19,44 +19,40 @@ class IntStack
             while (i<j)
             {
                 c=va_arg(ap,int);
-                init->push_back(c);
+                init.push_back(c);
                 i++;
             }
             va_end(ap);
             stack = init;
         }//renvoi un vecteur de taille a composé des valeurs mises en argument après a
 
-        ~IntStack ()
-        {
-            delete stack;
-        }
         void print()
         {
             std::cout << '[';
-            for (int i = 0; i < stack->size(); i++)
-                std::cout << (*stack)[i] << ' ';
+            for (int i = 0; i < stack.size(); i++)
+                std::cout << stack[i] << ' ';
             std::cout << '[' << std::endl;
         }
 
         void push(int i)
         {
-            stack->push_back(i);
+            stack.push_back(i);
         }
 
         int pop()
         {
-            int i = (*stack)[stack->size()-1];
-            stack->pop_back();
+            int i = stack[stack.size()-1];
+            stack.pop_back();
             return i;
         }
 
         bool is_empty()
         {
-            return stack->size()==0;
+            return stack.size()==0;
         }
 
     private:
   // vr: mets un objet directemetn comme cela tu évites de faire new
   // et delete std::vector<int> stack;
-        std::vector<int>* stack;
+        std::vector<int> stack;
 };
--- intstack/test_intstack.cpp
+++ intstack/test_intstack.cpp
@@ -1,34 +1,13 @@
+#include "intstack.h"
 #include <iostream>
-#include <vector>
 
-std::vector<int>* init_stack()
+int main()
 {
-    std::vector<int>* pi = new std::vector<int>;
-    return pi;
-}
-
-void delete_stack(std::vector<int>* stack)
-{
-    delete stack;
-}
-
-void print_stack(std::vector<int> stack)
-{
-    std::cout << '[';
-    for (int i = 0; i < stack.size(); i++)
-        std::cout << stack[i] << ' ';
-    std::cout << '[' << std::endl;
-}
-
-void push(std::vector<int>* tab, int i)
-{
-    (*tab).push_back(i);
-    print_stack(*tab);
-}
-
-int pop(std::vector<int>* tab)
-{
-    int i = (*tab)[(*tab).size()-1];
-    (*tab).pop_back();
-    return i;
+    IntStack stack = IntStack (5,4,2,7,3,74);
+    stack.push(2);
+    stack.print();
+    std::cout << stack.pop() << std::endl;
+    std::cout << stack.is_empty() << std::endl;
+    stack.print();
+    return 0;
 }
